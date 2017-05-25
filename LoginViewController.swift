ce61155b16d095e43c754e39170ae91037c6c755
Miguel <gannasong@gmail.com>
2017-05-25 13:25:42 +0800

    完成login頁面建置

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -8,28 +8,57 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController,UITextFieldDelegate {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
-
-     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+            return false
+        } else {
+            passwordTextField.resignFirstResponder()
+            return false
+        }
+        return true
     }
-    */
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+    //登入
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+    //忘記密碼
+    @IBAction func getPwButton(_ sender: UIButton) {
+    }
+    
+    
+    //轉場註冊頁
+    @IBAction func SignUpButton(_ sender: UIButton) {
+    }
+    
+    
 
+  //
 }
